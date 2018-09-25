f17ca97f2827b4e3d49852fd2e004fb36b84b202
Mazdak <mazdak.momen@gmail.com>
2018-09-25 17:14:01 -0400

    Add module for parsing the inputted log file

--- professional/log scraper/parser.py
+++ professional/log scraper/parser.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import re
 
 
 def arg_parse():
@@ -9,7 +10,7 @@ def arg_parse():
     parser.add_argument("log_path",
                         help="The directory path to scan files to be processed.")
     arguments = parser.parse_args()
-    if os.path.isdir(arguments.log_path):
+    if os.path.isfile(arguments.log_path):
         log_path = arguments.log_path
         return log_path
     else:
@@ -17,8 +18,29 @@ def arg_parse():
 
 
 def log_parse(log_path):
-    print log_path
+    pattern = r'(\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b) - - \[(.*)\] "(\w{3,5}) (\/.*) HTTP\/1.1" (\d{3}) (\d*)'
+    dict_ip = {}
+    dict_request_type = {}
+    dict_request_path = {}
+    dict_request_status_code = {}
+    # dict_datetime = {}
+    # dict_request_time = {}
+    i = 0
+
+    # loop over each line in log
+    with open(log_path) as f:
+        for line in f:
+            match = re.search(pattern, line)
+            dict_ip[i] = match.group(1)
+            dict_request_type[i] = match.group(3)
+            dict_request_path[i] = match.group(4)
+            dict_request_status_code[i] = match.group(5)
+            # dict_datetime[i] = match.group(2)
+            # dict_request_time[i] = match.group(6)
+            i += 1
+
+    return dict_ip, dict_request_type, dict_request_path, dict_request_status_code
 
 
 if __name__ == "__main__":
-    log_parse(arg_parse())
+    outputs = log_parse(arg_parse())
