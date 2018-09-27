ceccea204aab700c7cc225345ac9d1ad26491c53
Mazdak <mazdak.momen@gmail.com>
2018-09-27 08:20:40 -0400

    Multiple changes
    Update comments
    Change module names to better reflect function
    Change find_lines_of_string_in_dict_log to only return lines_found, using len() function to return count
    Update where above function is used so that variables are correct and len() is called as needed
    Update 4th case to handle multiple line numbers from one result

--- professional/log scraper/parser.py
+++ professional/log scraper/parser.py
@@ -45,9 +45,9 @@ def log_parse(log_path):
     return log_requests_ip, log_requests_type, log_requests_path, log_requests_status_code
 
 
-def find_count_of_string_in_dict(search_string, dict_log, search_string_unary=False):
+def find_lines_of_string_in_dict_log(search_string, dict_log, search_string_unary=False):
     ''' provided a string and a dictionary of values from the log, find string,
-        return number of appearances and an array of the line numbers where it was found
+        return the line numbers where string was found in log
     '''
 
     lines_found = []
@@ -59,22 +59,23 @@ def find_count_of_string_in_dict(search_string, dict_log, search_string_unary=Fa
         for line_num in dict_log:
             if search_string in dict_log[line_num]:
                 lines_found.append(line_num)
-    return len(lines_found), lines_found
+    return lines_found
 
 
-def find_count_of_values_of_lines_in_dict(arr_lines_found, dict_log):
-    ''' provided an array of line numbers and a dictionary of log values
-        return a dictionary of values of what appears on those lines
+def find_lines_of_given_array_of_lines_in_dict(arr_lines_found, dict_log):
+    ''' provided an array of line numbers and a dictionary of log values (ideally of a
+        different log type), return a dictionary of values of what appears on those lines
+        {"what is found": [line, numbers]}
     '''
 
-    dict_results = {}
+    results = {}
     for line_num in dict_log:
-        if dict_log[line_num] in dict_results:
-            dict_results[str(dict_log[line_num])].append(line_num)
+        if dict_log[line_num] in results:
+            results[str(dict_log[line_num])].append(line_num)
         else:
-            dict_results[str(dict_log[line_num])] = []
-            dict_results[str(dict_log[line_num])].append(line_num)
-    return dict_results
+            results[str(dict_log[line_num])] = []
+            results[str(dict_log[line_num])].append(line_num)
+    return results
 
 
 if __name__ == "__main__":
@@ -87,40 +88,40 @@ if __name__ == "__main__":
 
     # 1: How many times the URL "/production/file_metadata/modules/ssh/sshd_config" was fetched
     search_string = "/production/file_metadata/modules/ssh/sshd_config"
-    first_case = find_count_of_string_in_dict(search_string, request_paths)
-    first_case_count = first_case[0]
-    first_case_lines = first_case[1]
+    first_case_lines = find_lines_of_string_in_dict_log(search_string, request_paths)
+    first_case_count = len(first_case_lines)
     print str(first_case_count) + " appearances of the string string: " + search_string
 
     # 2: Of those requests, how many times the return code from Apache was not 200
-    second_case = find_count_of_values_of_lines_in_dict(first_case_lines, request_status_codes)
+    second_case = find_lines_of_given_array_of_lines_in_dict(first_case_lines, request_status_codes)
     for key in second_case:
         if key != "200":
             print key + " status code appeared " + str(len(second_case[key])) + " time(s)."
 
     # 3: The total number of times Apache returned any code other than 200
-    third_case = find_count_of_string_in_dict("200", request_status_codes, True)
-    third_case_count = third_case[0]
-    third_case_lines = third_case[1]
+    third_case_lines = find_lines_of_string_in_dict_log("200", request_status_codes, True)
+    third_case_count = len(third_case_lines)
     print str(third_case_count) + " appearances of non-200 HTTP status codes."
 
     # 4: The total number of times that any IP address sent a PUT request to a path under "/dev/report/"
     fourth_search_request = "PUT"
     fourth_search_path = "/dev/report/"
 
-    find_puts = find_count_of_string_in_dict(fourth_search_request, request_types)
-    find_puts_count = find_puts[0]  # Count of all PUTs
-    find_puts_lines = find_puts[1]  # Lines of all PUTs
+    find_puts_lines = find_lines_of_string_in_dict_log(fourth_search_request, request_types)
+    find_puts_count = len(find_puts_lines)
 
-    find_puts_by_path = find_count_of_values_of_lines_in_dict(find_puts_lines, request_paths)
+    find_puts_by_path = find_lines_of_given_array_of_lines_in_dict(find_puts_lines, request_paths)
     fourth_case_lines = []
     for key in find_puts_by_path:
         if fourth_search_path in key:
-            fourth_case_lines.append(find_puts_by_path[key])
+            # for loop here handles case of multiple line number results
+            for line_num in find_puts_by_path[key]:
+                fourth_case_lines.append(line_num)
+
     print str(len(fourth_case_lines)) + " appearances of " + fourth_search_request + " and " + fourth_search_path
 
     # 5: A breakdown of how many times such requests (from 4th case) were made by IP address
-    fifth_case = find_count_of_values_of_lines_in_dict(fourth_case_lines, request_ips)
+    fifth_case = find_lines_of_given_array_of_lines_in_dict(fourth_case_lines, request_ips)
     print "Breakdown by IP address of %s requests to a path under %s:" % (fourth_search_request, fourth_search_path)
     for key in fifth_case:
         print str(key) + ": " + str(len(fifth_case[key]))
