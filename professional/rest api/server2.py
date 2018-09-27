5fa42d5e02b68585d68d0bad2f9897036cf32213
Mazdak <mazdak.momen@gmail.com>
2018-09-27 17:10:42 -0400

    Page headers and cases around url levels
    Created module to handle status code and headers
    Updated case to handle proper URLs, watching for the lack or presence of trailling slash
    Proper code and header for multi-word error
    Write JSON object for error

--- professional/rest api/server2.py
+++ professional/rest api/server2.py
@@ -1,6 +1,6 @@
 import time
 import BaseHTTPServer
-# import urlparse
+import json
 
 # base code source: https://wiki.python.org/moin/BaseHttpServer
 
@@ -11,16 +11,17 @@ wordnames = {}
 
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def page_headers(self, code, content_type="text/html"):
+        self.send_response(code)
+        self.send_header("Content-type", content_type)
+        self.end_headers()
+
     def do_HEAD(s):
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.end_headers()
+        s.page_headers(200)
 
     def do_GET(s):
         """Respond to a GET request."""
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.end_headers()
+        s.page_headers(200)
         s.wfile.write("<html><head><title>Title goes here.</title></head>")
         s.wfile.write("<body><p>This is a test.</p>")
         # If someone went to "http://something.somewhere.net/foo/bar/",
@@ -30,9 +31,6 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def do_PUT(s):
         """Respond to a PUT request."""
-        s.send_response(200)
-        s.send_header("Content-type", "application/json")
-        s.end_headers()
         # On Success, return the integer count of how many times that word has been PUT to the api in a JSON hash
         path = s.path
         content_len = int(s.headers.getheader('content-length', 0))
@@ -41,19 +39,29 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         path_levels = len(path_split)-1  # first item is always empty (starting slash)
 
         if content_len == 0:  # check for nothing in request body
-            # check for 2 levels (3 items in list) or 3 levels with an empty string (trailing slash)
-            if (path_levels == 2) or (path_levels == 3 and path_split[-1] == ''):
-                print '2 levels'
+            # 2 levels and last is not an empty string (trailing slash with no WORDNAME) or
+            # 3 levels and last is an empty string (trailling slash)
+            if (path_levels == 2 and path_split[-1] != '') or (path_levels == 3 and path_split[-1] == ''):
                 path_first_level = path_split[1].lower()
                 path_second_level = path_split[2].lower()
                 if path_first_level == 'word':
-                    if path_second_level in wordnames:
-                        wordnames[path_second_level] += 1
-                    else:
-                        wordnames[path_second_level] = 1
-            else:  # all other cases do nothing
+                    if len(path_second_level.split('%20')) == 1:
+                        if path_second_level in wordnames:
+                            wordnames[path_second_level] += 1
+                            print wordnames
+                        else:
+                            wordnames[path_second_level] = 1
+                            print wordnames
+                    else:  # not one word, return json hash and http error code
+                        print 'wordname is not 1 word'
+                        json_string = json.dumps({"error": "PUT requests must be one word in length"})
+                        s.page_headers(400, "application/json")
+                        s.wfile.write(json_string)
+                else:  # first level is not 'word'
+                    print 'first level must be \'word\''
+            else:  # path is not expected
                 print 'path is not correct'
-        else:
+        else:  # request has a body
             print 'must have no request body'  # else ignore
 
 if __name__ == '__main__':
