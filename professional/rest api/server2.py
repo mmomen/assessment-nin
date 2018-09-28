600fb88f4fc9b983daaddf143b2411af2251f996
Mazdak <mazdak.momen@gmail.com>
2018-09-28 11:35:47 -0400

    Refactor into page_header to handle headers and client returns out of main branching.

--- professional/rest api/server2.py
+++ professional/rest api/server2.py
@@ -1,6 +1,7 @@
 import time
 import BaseHTTPServer
 import json
+import string
 
 # base code source: https://wiki.python.org/moin/BaseHttpServer
 
@@ -11,17 +12,17 @@ wordnames = {}
 
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def page_headers(self, code, content_type="text/html"):
+    def page_headers(self, code, content_type):
         self.send_response(code)
         self.send_header("Content-type", content_type)
         self.end_headers()
 
     def do_HEAD(s):
-        s.page_headers(200)
+        s.page_headers(200, "text/html")
 
     def do_GET(s):
         """Respond to a GET request."""
-        s.page_headers(200)
+        s.page_headers(200, "text/html")
         s.wfile.write("<html><head><title>Title goes here.</title></head>")
         s.wfile.write("<body><p>This is a test.</p>")
         # If someone went to "http://something.somewhere.net/foo/bar/",
@@ -32,11 +33,42 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_PUT(s):
         """Respond to a PUT request."""
         # On Success, return the integer count of how many times that word has been PUT to the api in a JSON hash
+        invalidChars = set(string.punctuation)
         path = s.path
         content_len = int(s.headers.getheader('content-length', 0))
         body = s.rfile.read(content_len)
         path_split = path.split('/')
         path_levels = len(path_split)-1  # first item is always empty (starting slash)
+
+        def return_request(code, content_type, message):
+            s.page_headers(code, content_type)
+            if content_type == "application/json":
+                message_to_write = json.dumps(message)
+            else:  # TK is this needed?
+                print 'is this needed?'
+            s.wfile.write(message_to_write)
+
+        def return_single(wordname, word_log):
+            if wordname in word_log:
+                word_log[wordname] += 1
+            else:
+                word_log[wordname] = 1
+            return_request(200, "application/json", {"words": {wordname: word_log[wordname]}})
+
+        def return_error(error_type, code):
+            if error_type == "special character":
+                error_message = "PUT requests may not contain special characters."
+            elif error_type == "not one word":
+                error_message = "PUT requests must be one word (e.g. /word/christopherwalken)."
+            elif error_type == "first level not word":
+                error_message = "PUT requests must use 'word' as first level in path (e.g. /word/[WORDNAME])."
+            elif error_type == "bad path":
+                error_message = "PUT requests must have paths in the following structure: /word/[WORDNAME])"
+            elif error_type == "request has body":
+                error_message = "PUT requests cannot contain a request body."
+            else:
+                error_message = "An unexpected error has occured."
+            return_request(code, "application/json", {"error": error_message})
 
         if content_len == 0:  # check for nothing in request body
             # 2 levels and last is not an empty string (trailing slash with no WORDNAME) or
@@ -44,25 +76,21 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             if (path_levels == 2 and path_split[-1] != '') or (path_levels == 3 and path_split[-1] == ''):
                 path_first_level = path_split[1].lower()
                 path_second_level = path_split[2].lower()
-                if path_first_level == 'word':
+                if path_first_level == "word":
                     if len(path_second_level.split('%20')) == 1:
-                        if path_second_level in wordnames:
-                            wordnames[path_second_level] += 1
-                            print wordnames
+                        if any(char in invalidChars for char in path_second_level):  # if no special characters
+                            return_error("special character", 400)
                         else:
-                            wordnames[path_second_level] = 1
-                            print wordnames
-                    else:  # not one word, return json hash and http error code
-                        print 'wordname is not 1 word'
-                        json_string = json.dumps({"error": "PUT requests must be one word in length"})
-                        s.page_headers(400, "application/json")
-                        s.wfile.write(json_string)
-                else:  # first level is not 'word'
-                    print 'first level must be \'word\''
-            else:  # path is not expected
-                print 'path is not correct'
-        else:  # request has a body
-            print 'must have no request body'  # else ignore
+                            return_single(path_second_level, wordnames)
+                    else:
+                        return_error("not one word", 400)
+                else:
+                    return_error("first level not word", 400)
+            else:
+                return_error("bad path", 400)
+        else:
+            return_error("request has body", 400)
+
 
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
