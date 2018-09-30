3e995a178d7c653a5fc00bc880c7113998ca3b91
Mazdak <mazdak.momen@gmail.com>
2018-09-30 11:04:56 -0400

    Various changes
    Improved comments
    Previous logic for find_lines_of_given_array_of_lines_in_dict was incorrect, this has been corrected to
    search within the dict_log using the given line numbers instead of running through the entire log.
    Improved output of the case answers.
    Updated 2nd case logic to search for non-200 codes.
    Gave a more specific var name to the search string in the first case.

--- professional/log scraper/parser.py
+++ professional/log scraper/parser.py
@@ -4,6 +4,9 @@ import sys
 import re
 
 
+'''Run script: `python parser.py <path-to-log-file>`'''
+
+
 def arg_parse():
     parser = argparse.ArgumentParser(
         description="")
@@ -18,8 +21,8 @@ def arg_parse():
 
 
 def log_parse(log_path):
-    ''' parse log to pull relevant data, will be saved in a dictionary
-        key is line number, value is the desired value for that line
+    ''' Use regex to parse log. Save data into relevant dictionaries.
+        Key will be line number, value is the respective data type (IP, time, etc...) on that line.
     '''
 
     pattern = r'(\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b) - - \[(.*)\] "(\w{3,5}) (\/.*) HTTP\/1.1" (\d{3}) (\d*)'
@@ -46,8 +49,8 @@ def log_parse(log_path):
 
 
 def find_lines_of_string_in_dict_log(search_string, dict_log, search_string_unary=False):
-    ''' provided a string and a dictionary of values from the log, find string,
-        return the line numbers where string was found in log
+    ''' Given a string and a dictionary (log of specific data type from log), find the string,
+        and return the line numbers where string was found in log.
     '''
 
     lines_found = []
@@ -63,18 +66,18 @@ def find_lines_of_string_in_dict_log(search_string, dict_log, search_string_unar
 
 
 def find_lines_of_given_array_of_lines_in_dict(arr_lines_found, dict_log):
-    ''' provided an array of line numbers and a dictionary of log values (ideally of a
-        different log type), return a dictionary of values of what appears on those lines
-        {"found_value": [1, 5, 95]}
+    ''' Given an array of line numbers from the log, and a dictionary of log values (of a
+        different type from the log), return a dictionary of values of what appears the lines of the
+        given dictionary. For example: {"/dev/report/ec2-54-211-240-78.compute-1.amazonaws.com": [0]}
     '''
 
     results = {}
-    for line_num in dict_log:
-        if dict_log[line_num] in results:
-            results[str(dict_log[line_num])].append(line_num)
+    for given_line_num in arr_lines_found:
+        if dict_log[given_line_num] in results:
+            results[str(dict_log[given_line_num])].append(given_line_num)
         else:
-            results[str(dict_log[line_num])] = []
-            results[str(dict_log[line_num])].append(line_num)
+            results[str(dict_log[given_line_num])] = []
+            results[str(dict_log[given_line_num])].append(given_line_num)
     return results
 
 
@@ -86,24 +89,33 @@ if __name__ == "__main__":
     request_paths = outputs[2]
     request_status_codes = outputs[3]
 
-    # 1: How many times the URL "/production/file_metadata/modules/ssh/sshd_config" was fetched
-    search_string = "/production/file_metadata/modules/ssh/sshd_config"
-    first_case_lines = find_lines_of_string_in_dict_log(search_string, request_paths)
+    print "1: How many times the URL '/production/file_metadata/modules/ssh/sshd_config' was fetched?"
+    first_case_search_string = "/production/file_metadata/modules/ssh/sshd_config"
+    first_case_lines = find_lines_of_string_in_dict_log(first_case_search_string, request_paths)
     first_case_count = len(first_case_lines)
-    print str(first_case_count) + " appearances of the string string: " + search_string
+    print str(first_case_count) + " appearances of the string: " + first_case_search_string
 
-    # 2: Of those requests, how many times the return code from Apache was not 200
+    print "---"
+
+    print "2: Of those requests, how many times the return code from Apache was not 200?"
     second_case = find_lines_of_given_array_of_lines_in_dict(first_case_lines, request_status_codes)
+    not_200 = 0
     for key in second_case:
         if key != "200":
-            print key + " status code appeared " + str(len(second_case[key])) + " time(s)."
+            not_200 += len(second_case[key])
 
-    # 3: The total number of times Apache returned any code other than 200
+    print "%i instances of %s status codes returned from results of first case." % (not_200, "non-200")
+
+    print "---"
+
+    print "3: The total number of times Apache returned any code other than 200?"
     third_case_lines = find_lines_of_string_in_dict_log("200", request_status_codes, True)
     third_case_count = len(third_case_lines)
     print str(third_case_count) + " appearances of non-200 HTTP status codes."
 
-    # 4: The total number of times that any IP address sent a PUT request to a path under "/dev/report/"
+    print "---"
+
+    print "4: The total number of times that any IP address sent a PUT request to a path under '/dev/report/'?"
     fourth_search_request = "PUT"
     fourth_search_path = "/dev/report/"
 
@@ -120,7 +132,9 @@ if __name__ == "__main__":
 
     print str(len(fourth_case_lines)) + " appearances of " + fourth_search_request + " and " + fourth_search_path
 
-    # 5: A breakdown of how many times such requests (from 4th case) were made by IP address
+    print "---"
+
+    print "5: A breakdown of how many times such requests (from 4th case) were made by IP address?"
     fifth_case = find_lines_of_given_array_of_lines_in_dict(fourth_case_lines, request_ips)
     print "Breakdown by IP address of %s requests to a path under %s:" % (fourth_search_request, fourth_search_path)
     for key in fifth_case:
